0f4f1188a1c4c0c1d2667036b6f5be661576e3bc
TrongNhannn <72992044+TrongNhannn@users.noreply.github.com>
2023-04-16 21:00:12 +0700

    test log

--- server/route/logs.js
+++ server/route/logs.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
+
 const logger = morgan('combined', { stream: accessLogStream });
 
 module.exports = logger;
